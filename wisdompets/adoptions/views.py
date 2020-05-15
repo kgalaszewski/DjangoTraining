ba233372b70b3f245c874191ff1c5c46dea17dfb
Karol <kgalaszewski95@gmail.com>
2020-05-15 22:03:06 +0100

    finished the projects with some additional js

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+from .models import Pet, Vaccine
 
-# Create your views here.
+def home(request):
+    try:
+        allpets = Pet.objects.all()
+    except:
+        raise Http404('we could not load pets for you')
+    return render(request, 'home.html', {
+            'pets': allpets,
+            'testArg': 2137
+        })
+
+def pet_detail(request, pet_id: int):
+    try:
+        pet = Pet.objects.get(id=pet_id)
+        return render(request, 'pet_detail.html', {
+            'pet': pet
+        }) # request obj, name of html page, context to pass
+    except Pet.DoesNotExist:
+        return home(request)
