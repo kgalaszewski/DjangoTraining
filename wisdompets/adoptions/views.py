b91c07474a89186354bec98469ae04358c445d99
Karol Gałaszewski <kgalaszewski95@gmail.com>
2020-05-16 03:08:51 +0200

    implemented db insert for pet - needs refactoring

--- wisdompets/adoptions/views.py
+++ wisdompets/adoptions/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from .models import Pet, Vaccine
+from django.db import models
+
 
 def home(request):
     try:
@@ -8,15 +10,32 @@ def home(request):
     except:
         raise Http404('we could not load pets for you')
     return render(request, 'home.html', {
-            'pets': allpets,
-            'testArg': 2137
-        })
+        'pets': allpets,
+        'testArg': 2137
+    })
+
 
 def pet_detail(request, pet_id: int):
     try:
         pet = Pet.objects.get(id=pet_id)
         return render(request, 'pet_detail.html', {
             'pet': pet
-        }) # request obj, name of html page, context to pass
+        })  # request obj, name of html page, context to pass
     except Pet.DoesNotExist:
         return home(request)
+
+
+def create_pet_get(request):
+    pet = Pet(name='random', breed='random')
+    return render(request, 'create_pet.html', {'pet': pet})
+
+
+def create_pet_post(request, pet_name):
+    try:
+        pet2 = Pet(name=pet_name, submitter='asd', species='asdasd', breed='asd',
+                   description='asdasd', sex='M', submission_date='2007-01-01 10:00:00')
+        pet2.save()
+    except:
+        return render(request, 'dupa.html')
+        # return Http404('we could not create a pet')
+    return home(request)
--- wisdompets/wisdompets/urls.py
+++ wisdompets/wisdompets/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     # path('URL/param', 'view name / def name', '???')
-    path('adoptions/pets/<int:pet_id>/', views.pet_detail, name='pet_detail')
+    path('adoptions/pets/<int:pet_id>/', views.pet_detail, name='pet_detail'),
+    path('adoptions/pets/create/', views.create_pet_get, name='create_pet_get'),
+    path('adoptions/pets/insert/<str:pet_name>/',
+         views.create_pet_post, name='create_pet_post')
 ]
